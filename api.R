d91785d89be8353c5551572f8469d3563f491a23
Miguel Veliz <mwveliz@gmail.com>
2020-03-02 22:12:01 -0300

    htm file with readr

--- api.R
+++ api.R
@@ -9,17 +9,24 @@
 #* @description Clasifica clientes
 clasifica_clientes = function(req) {
   input = jsonlite::fromJSON(req$postBody)
-  print(input)
+  #print(input)
   #logger("las col son:")
   #logger(names(input))
   output = summary(input)
-  print("Dataset de entrada:")
+  readr::write_file("Inicializando Rscript modelo.", "/tmp/capstone.html")
+  readr::write_file("Dataset de entrada:", append = TRUE, "/tmp/capstone.html")
   input_hux <- 
     hux(input) %>% 
     add_colnames() %>% 
     set_bold(row = 1, col = everywhere, value = TRUE) %>% 
     set_all_borders(TRUE)
   print_screen(input_hux)
-
-  return(output)
+  
+  kable_out <- knitr::kable(input, "html") %>% kableExtra::kable_styling(bootstrap_options = c("striped", "hover"))
+  readr::write_file(kable_out, append = TRUE, "/tmp/capstone.html")
+  readr::write_file("Finaliza", append = TRUE, "/tmp/capstone.html")
+  
+  
+  
+  return('/tmp/capstone.html')
 }
